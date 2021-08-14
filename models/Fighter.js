1f14e13193cd5130ba9231abfcee01a9a2e79166
Mauricio <mauriciodelcas30@gmail.com>
2021-08-13 20:24:32 -0400

    Reorganized FighterSchema

--- models/Fighter.js
+++ models/Fighter.js
@@ -1,49 +1,55 @@
 const mongoose = require("mongoose");
 
 const FighterSchema = mongoose.Schema({
-  firstname: {
-    required: true,
-    type: String,
+  info: {
+    firstname: {
+      required: true,
+      type: String,
+    },
+    lastname: {
+      required: true,
+      type: String,
+    },
+    nickname: {
+      required: false,
+      type: String,
+    },
   },
-  lastname: {
-    required: true,
-    type: String,
+  stats: {
+    height: {
+      required: true,
+      type: String,
+    },
+    weight: {
+      required: true,
+      type: String,
+    },
+    reach: {
+      required: true,
+      type: String,
+    },
+    stance: {
+      required: true,
+      type: String,
+    },
   },
-  nickname: {
-    required: false,
-    type: String,
-  },
-  height: {
-    required: true,
-    type: String,
-  },
-  weight: {
-    required: true,
-    type: String,
-  },
-  reach: {
-    required: true,
-    type: String,
-  },
-  stance: {
-    required: true,
-    type: String,
-  },
-  wins: {
-    required: true,
-    type: Number,
-  },
-  losses: {
-    required: true,
-    type: Number,
-  },
-  draws: {
-    required: true,
-    type: Number,
-  },
-  belt: {
-    required: true,
-    type: Boolean,
+  career: {
+    wins: {
+      required: true,
+      type: Number,
+    },
+    losses: {
+      required: true,
+      type: Number,
+    },
+    draws: {
+      required: true,
+      type: Number,
+    },
+    belt: {
+      required: true,
+      type: Boolean,
+    },
   },
 });
 
