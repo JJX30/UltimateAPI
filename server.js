4b13780c9afd68ca2ec486df2d1195157b387cb3
Mauricio <mauriciodelcas30@gmail.com>
2021-08-31 01:02:22 -0400

    Added query by id, stance, record, firstname, and lastname

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
 const Fighter = require("./models/Fighter.js");
@@ -22,30 +23,351 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-//ROUTES
-
-app.get("/v1/fighters", async (req, res) => {
+// find fighter based on firstname lastname query
+app.get("/v1/ufc/fighters", async (req, res) => {
+  const queries = Object.keys(req.query);
+  const { firstname, lastname } = req.query;
   try {
-    const result = await Fighter.find();
-    res.status(200).send(result);
+    if (firstname && lastname) {
+      const fighterInfo = await Fighter.find({
+        firstname: new RegExp(firstname, "i"),
+        lastname: new RegExp(lastname, "i"),
+      });
+      if (fighterInfo.length !== 0) {
+        res.status(200).json(fighterInfo);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (firstname) {
+      const fighterInfo = await Fighter.find({
+        firstname: new RegExp(firstname, "i"),
+      });
+      if (fighterInfo.length !== 0) {
+        res.status(200).json(fighterInfo);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (lastname) {
+      const fighterInfo = await Fighter.find({
+        lastname: new RegExp(lastname, "i"),
+      });
+      if (fighterInfo.length !== 0) {
+        res.status(200).json(fighterInfo);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (queries.length === 0) {
+      res.status(400).json({
+        status: {
+          message: "invalid input",
+          code: 400,
+        },
+      });
+    } else {
+      res.status(400).json({
+        status: {
+          message: "invalid input",
+          code: 400,
+        },
+      });
+    }
   } catch (err) {
     console.log(err);
-    res.status(404).send("Something went wrong!");
+    res.status(500).json({
+      status: {
+        message: "internal server error",
+        code: 500,
+      },
+    });
+  }
+});
+// get fighter list based off stances
+app.get("/v1/ufc/fighters/stance", async (req, res) => {
+  const { stance } = req.query;
+
+  try {
+    if (stance) {
+      const fighterInfo = await Fighter.find({
+        stance: new RegExp(stance, "i"),
+      });
+      res.status(200).json(fighterInfo);
+    } else {
+      res.status(400).json({
+        status: {
+          message: "invalid input",
+          code: 400,
+        },
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: {
+        message: "internal server error",
+        code: 500,
+      },
+    });
+  }
+});
+// get fighter list based off number of wins
+app.get("/v1/ufc/fighters/record", async (req, res) => {
+  const { wins, losses, draws } = req.query;
+  const queries = Object.keys(req.query);
+
+  try {
+    if (wins && losses && draws) {
+      console.log("wins,losses,draws");
+      const fighterInfo = await Fighter.find({
+        wins: wins,
+        losses: losses,
+        draws: draws,
+      });
+      if (fighterInfo.length !== 0) {
+        res.status(200).json(fighterInfo);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (wins && losses) {
+      const fighterInfo = await Fighter.find({
+        wins: wins,
+        losses: losses,
+      });
+      if (fighterInfo.length !== 0) {
+        res.status(200).json(fighterInfo);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (losses && draws) {
+      const fighterInfo = await Fighter.find({
+        losses: losses,
+        draws: draws,
+      });
+      if (fighterInfo.length !== 0) {
+        res.status(200).json(fighterInfo);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (wins && draws) {
+      const fighterInfo = await Fighter.find({
+        wins: wins,
+        draws: draws,
+      });
+      if (fighterInfo.length !== 0) {
+        res.status(200).json(fighterInfo);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (wins) {
+      const fighterInfo = await Fighter.find({
+        wins: wins,
+      });
+      if (fighterInfo.length !== 0) {
+        res.status(200).json(fighterInfo);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (losses) {
+      const fighterInfo = await Fighter.find({
+        losses: losses,
+      });
+      if (fighterInfo.length !== 0) {
+        res.status(200).json(fighterInfo);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (draws) {
+      const fighterInfo = await Fighter.find({
+        draws: draws,
+      });
+      if (fighterInfo.length !== 0) {
+        res.status(200).json(fighterInfo);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (queries.length === 0) {
+      res.status(400).json({
+        status: {
+          message: "invalid input",
+          code: 400,
+        },
+      });
+    } else {
+      res.status(400).json({
+        status: {
+          message: "invalid input",
+          code: 400,
+        },
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: {
+        message: "internal server error",
+        code: 500,
+      },
+    });
+  }
+});
+// fighter ID's based off firstname and lastname query
+app.get("/v1/ufc/fighters/id", async (req, res) => {
+  const { firstname, lastname } = req.query;
+  const queries = req.query;
+  try {
+    if (firstname && lastname) {
+      const fighterInfo = await Fighter.find({
+        firstname: new RegExp(firstname, "i"),
+        lastname: new RegExp(lastname, "i"),
+      });
+      const idList = [];
+      fighterInfo.forEach((fighter) => {
+        idList.push({
+          _id: fighter._id,
+        });
+      });
+      if (idList.length !== 0) {
+        res.status(200).json(idList);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (firstname) {
+      const fighterInfo = await Fighter.find({
+        firstname: new RegExp(firstname, "i"),
+      });
+      const fighterList = [];
+      fighterInfo.forEach((fighter) => {
+        fighterList.push({
+          _id: fighter._id,
+        });
+      });
+      if (idList.length !== 0) {
+        res.status(200).json(idList);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (lastname) {
+      const fighterInfo = await Fighter.find({
+        lastname: new RegExp(lastname, "i"),
+      });
+      const fighterList = [];
+      fighterInfo.forEach((fighter) => {
+        fighterList.push({
+          _id: fighter._id,
+        });
+      });
+      if (idList.length !== 0) {
+        res.status(200).json(idList);
+      } else {
+        res.status(204).json({
+          status: {
+            message: "no content",
+            code: 204,
+          },
+        });
+      }
+    } else if (queries.length === 0) {
+      res.status(400).json({
+        status: {
+          message: "invalid input",
+          code: 400,
+        },
+      });
+    } else {
+      res.status(400).json({
+        status: {
+          message: "invalid input",
+          code: 400,
+        },
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: {
+        message: "internal server error",
+        code: 500,
+      },
+    });
   }
 });
 
-//ROUTES by ID
-app.get("/v1/fighters/:id", async (req, res) => {
+// ROUTES by ID
+app.get("/v1/ufc/fighters/:id", async (req, res) => {
   try {
     const fighterInfo = await Fighter.findOne({ _id: req.params.id });
     res.status(200).json(fighterInfo);
   } catch (err) {
     console.log(err);
-    res.status(404).send("Could not find by ID!");
+    res.status(404).json({
+      status: {
+        message: "not found",
+        code: 404,
+      },
+    });
   }
 });
 
-app.get("/v1/fighters/:id/firstname", async (req, res) => {
+app.get("/v1/ufc/fighters/:id/firstname", async (req, res) => {
   try {
     const result = await Fighter.findOne({ _id: req.params.id }).lean();
     const response = {
@@ -54,11 +376,16 @@ app.get("/v1/fighters/:id/firstname", async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     console.log(err);
-    res.status(404).send("Could not find by ID!");
+    res.status(404).json({
+      status: {
+        message: "not found",
+        code: 404,
+      },
+    });
   }
 });
 
-app.get("/v1/fighters/:id/lastname", async (req, res) => {
+app.get("/v1/ufc/fighters/:id/lastname", async (req, res) => {
   try {
     const result = await Fighter.findOne({ _id: req.params.id }).lean();
     const response = {
@@ -67,10 +394,15 @@ app.get("/v1/fighters/:id/lastname", async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     console.log(err);
-    res.status(404).send("Could not find by ID!");
+    res.status(404).json({
+      status: {
+        message: "not found",
+        code: 404,
+      },
+    });
   }
 });
-app.get("/v1/fighters/:id/nickname", async (req, res) => {
+app.get("/v1/ufc/fighters/:id/nickname", async (req, res) => {
   try {
     const result = await Fighter.findOne({ _id: req.params.id }).lean();
     const response = {
@@ -79,11 +411,16 @@ app.get("/v1/fighters/:id/nickname", async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     console.log(err);
-    res.status(404).send("Could not find by ID!");
+    res.status(404).json({
+      status: {
+        message: "not found",
+        code: 404,
+      },
+    });
   }
 });
 
-app.get("/v1/fighters/:id/height", async (req, res) => {
+app.get("/v1/ufc/fighters/:id/height", async (req, res) => {
   try {
     const result = await Fighter.findOne({ _id: req.params.id }).lean();
     const response = {
@@ -92,11 +429,16 @@ app.get("/v1/fighters/:id/height", async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     console.log(err);
-    res.status(404).send("Could not find by ID!");
+    res.status(404).json({
+      status: {
+        message: "not found",
+        code: 404,
+      },
+    });
   }
 });
 
-app.get("/v1/fighters/:id/weight", async (req, res) => {
+app.get("/v1/ufc/fighters/:id/weight", async (req, res) => {
   try {
     const result = await Fighter.findOne({ _id: req.params.id }).lean();
     const response = {
@@ -105,11 +447,16 @@ app.get("/v1/fighters/:id/weight", async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     console.log(err);
-    res.status(404).send("Could not find by ID!");
+    res.status(404).json({
+      status: {
+        message: "not found",
+        code: 404,
+      },
+    });
   }
 });
 
-app.get("/v1/fighters/:id/reach", async (req, res) => {
+app.get("/v1/ufc/fighters/:id/reach", async (req, res) => {
   try {
     const result = await Fighter.findOne({ _id: req.params.id }).lean();
     const response = {
@@ -118,11 +465,16 @@ app.get("/v1/fighters/:id/reach", async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     console.log(err);
-    res.status(404).send("Could not find by ID!");
+    res.status(404).json({
+      status: {
+        message: "not found",
+        code: 404,
+      },
+    });
   }
 });
 
-app.get("/v1/fighters/:id/stance", async (req, res) => {
+app.get("/v1/ufc/fighters/:id/stance", async (req, res) => {
   try {
     const result = await Fighter.findOne({ _id: req.params.id }).lean();
     const response = {
@@ -131,11 +483,16 @@ app.get("/v1/fighters/:id/stance", async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     console.log(err);
-    res.status(404).send("Could not find by ID!");
+    res.status(404).json({
+      status: {
+        message: "not found",
+        code: 404,
+      },
+    });
   }
 });
 
-app.get("/v1/fighters/:id/wins", async (req, res) => {
+app.get("/v1/ufc/fighters/:id/wins", async (req, res) => {
   try {
     const result = await Fighter.findOne({ _id: req.params.id }).lean();
     const response = {
@@ -144,11 +501,16 @@ app.get("/v1/fighters/:id/wins", async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     console.log(err);
-    res.status(404).send("Could not find by ID!");
+    res.status(404).json({
+      status: {
+        message: "not found",
+        code: 404,
+      },
+    });
   }
 });
 
-app.get("/v1/fighters/:id/losses", async (req, res) => {
+app.get("/v1/ufc/fighters/:id/losses", async (req, res) => {
   try {
     const result = await Fighter.findOne({ _id: req.params.id }).lean();
     const response = {
@@ -157,11 +519,16 @@ app.get("/v1/fighters/:id/losses", async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     console.log(err);
-    res.status(404).send("Could not find by ID!");
+    res.status(404).json({
+      status: {
+        message: "not found",
+        code: 404,
+      },
+    });
   }
 });
 
-app.get("/v1/fighters/:id/draws", async (req, res) => {
+app.get("/v1/ufc/fighters/:id/draws", async (req, res) => {
   try {
     const result = await Fighter.findOne({ _id: req.params.id }).lean();
     const response = {
@@ -170,11 +537,16 @@ app.get("/v1/fighters/:id/draws", async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     console.log(err);
-    res.status(404).send("Could not find by ID!");
+    res.status(404).json({
+      status: {
+        message: "not found",
+        code: 404,
+      },
+    });
   }
 });
 
-app.get("/v1/fighters/:id/belt", async (req, res) => {
+app.get("/v1/ufc/fighters/:id/belt", async (req, res) => {
   try {
     const result = await Fighter.findOne({ _id: req.params.id }).lean();
     const response = {
@@ -183,76 +555,11 @@ app.get("/v1/fighters/:id/belt", async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     console.log(err);
-    res.status(404).send("Could not find by ID!");
+    res.status(404).json({
+      status: {
+        message: "not found",
+        code: 404,
+      },
+    });
   }
 });
-
-app.get("/v1/fighters/find", async (req, res) => {
-  switch (req.query) {
-    case req.query.firstname:
-      const queryFirstname = req.query.firstname;
-      const result = await Fighter.find({ firstname: queryFirstname });
-      res.status(200).json(result);
-
-    case req.query.lastname:
-      const queryLastname = req.query.lastname;
-      const result = await Fighter.find({ firstname: queryLastname });
-      res.status(200).json(result);
-
-    case req.query.nickname:
-      const queryNickname = req.query.nickname;
-      const result = await Fighter.find({ firstname: queryNickname });
-      res.status(200).json(result);
-
-    case req.query.height:
-      const queryHeight = req.query.nickname;
-      const result = await Fighter.find({ firstname: queryHeight });
-      res.status(200).json(result);
-
-    case req.query.weight:
-      const queryWeight = req.query.weight;
-      const result = await Fighter.find({ firstname: queryWeight });
-      res.status(200).json(result);
-
-    case req.query.reach:
-      const queryReach = req.query.weight;
-      const result = await Fighter.find({ firstname: queryReach });
-      res.status(200).json(result);
-
-    case req.query.stance:
-      const queryStance = req.query.stance;
-      const result = await Fighter.find({ firstname: queryStance });
-      res.status(200).json(result);
-
-    case req.query.wins:
-      const queryWins = req.query.wins;
-      const result = await Fighter.find({ firstname: queryWins });
-      res.status(200).json(result);
-
-    case req.query.losses:
-      const queryLosses = req.query.losses;
-      const result = await Fighter.find({ firstname: queryLosses });
-      res.status(200).json(result);
-
-    case req.query.draws:
-      const queryDraws = req.query.draws;
-      const result = await Fighter.find({ firstname: queryDraws });
-      res.status(200).json(result);
-
-    case req.query.belt:
-      const queryBelt = req.query.belt;
-      const result = await Fighter.find({ firstname: queryBelt });
-      res.status(200).json(result);
-  }
-});
-
-// if (req.query.lastname) {
-//   try {
-//     const result = await Fighter.find({
-//       lastname: { $regex: new RegExp(req.query.firstname, "i") },
-//     });
-//     res.status(200).json(result);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(404).send("Could not find fighter by queries");
-//   }
